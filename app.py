f220e757bd3ec3ebde1f5766e88a0ea38cef7987
Isaac Collins <isaacmcollins12@gmail.com>
2021-01-13 21:14:45 -0800

    init

---
+++ app.py
@@ -0,0 +1,29 @@
+import flask
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+app.config["DEBUG"]=True
+
+sample_data = [
+    {
+        "id": 1,
+        "data": "data1"
+    },
+    {
+        "id": 2,
+        "data": "data2"
+    }
+
+]
+
+@app.route('/sample', methods=["GET"])
+def api_sample():
+    if id in request.args:
+        id = int(request.args["id"])
+    else:
+        print("error")
+    resp = []
+    for data in sample_data:
+        if data['id'] == id:
+            resp.append(data)
+    return jsonify(resp)
